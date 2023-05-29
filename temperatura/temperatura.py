60beaff9bfdcab9d425c8b58de6191cd022c108c
Angel Garcia <92334014+Yusset02@users.noreply.github.com>
2023-05-29 10:40:19 -0700

    Update temperatura.py

--- temperatura/temperatura.py
+++ temperatura/temperatura.py
@@ -1,1 +1,11 @@
+from machine import ADC
+import time
 
+sensor = ADC(4)
+
+
+while True:
+    valor = sensor.read_u16()*3.3/65535
+    temp = 30-(valor-0.706)/0.001721
+    print(temp)
+    time.sleep(4)
